809d9911c7c413177dd2d92aa67993731543336a
Nazar Laba <35496104+fardo9@users.noreply.github.com>
2025-05-31 15:03:45 +0200

    add shared/ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,5 @@
-'use client'
-
 import React from 'react'
 
 export default function HomePage() {
-  const handleClick = () => alert('Lol')
-  return <div>page</div>
+  return <div>HomePage</div>
 }
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,43 +4,43 @@ export interface IProps {
   className?: string
 }
 
-type Item = FilterChecboxProps
+// type Item = FilterChecboxProps
 
-export interface FilterChecboxProps {
-  text: string
-  value: string
-  endAdornment?: React.ReactNode
-  onCheckedChange?: (checked: boolean) => void
-  checked?: boolean
-  name?: string
-}
+// export interface FilterChecboxProps {
+//   text: string
+//   value: string
+//   endAdornment?: React.ReactNode
+//   onCheckedChange?: (checked: boolean) => void
+//   checked?: boolean
+//   name?: string
+// }
 
-export interface ICheckboxFilterGroupProps {
-  title: string
-  items: Item[]
-  defaultItems?: Item[]
-  limit?: number
-  isLoading?: boolean
-  isError?: boolean
-  error?: any
-  searchInputPlaceholder?: string
-  onClickCheckbox?: (id: string) => void
-  defaultValue?: string[]
-  selected?: Set<string>
-  className?: string
-  name?: string
-}
+// export interface ICheckboxFilterGroupProps {
+//   title: string
+//   items: Item[]
+//   defaultItems?: Item[]
+//   limit?: number
+//   isLoading?: boolean
+//   isError?: boolean
+//   error?: any
+//   searchInputPlaceholder?: string
+//   onClickCheckbox?: (id: string) => void
+//   defaultValue?: string[]
+//   selected?: Set<string>
+//   className?: string
+//   name?: string
+// }
 
-export interface IProductsGroupListProps {
-  title: string
-  items: any[]
-  categoryId: number
-  listClassName?: string
-  className?: string
-}
+// export interface IProductsGroupListProps {
+//   title: string
+//   items: any[]
+//   categoryId: number
+//   listClassName?: string
+//   className?: string
+// }
 
-export interface IIngredients extends IProps {
-  //   ingredient: Ingredient
-  active?: boolean
-  onClick?: () => void
-}
+// export interface IIngredients extends IProps {
+//     ingredient: Ingredient
+//   active?: boolean
+//   onClick?: () => void
+// }
