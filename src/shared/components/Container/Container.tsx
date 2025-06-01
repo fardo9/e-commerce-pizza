1876b79f5207de94fe26e0cceedf69d7756f15dc
Nazar Laba <35496104+fardo9@users.noreply.github.com>
2025-06-01 14:40:23 +0200

    fix: normalize casing of container

--- src/shared/components/Container/Container.tsx
+++
@@ -1,8 +0,0 @@
-import type { PropsWithChildren } from 'react'
-
-import { cn } from '@shared/lib/utils'
-import type { IProps } from '@shared/types'
-
-export function Container({ className, children }: PropsWithChildren<IProps>) {
-  return <div className={cn('mx-auto max-w-[1280px]', className)}>{children}</div>
-}
