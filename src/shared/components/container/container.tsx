cf07944971c93006a60def2d1d9201a5b9528fbc
Nazar Laba <35496104+fardo9@users.noreply.github.com>
2025-06-01 15:15:43 +0200

    fix: remove duplicate Container.tsx

--- src/shared/components/container/container.tsx
+++ src/shared/components/container/container.tsx
@@ -6,3 +6,4 @@ import type { IProps } from '@shared/types'
 export function Container({ className, children }: PropsWithChildren<IProps>) {
   return <div className={cn('mx-auto max-w-[1280px]', className)}>{children}</div>
 }
+//  .,
